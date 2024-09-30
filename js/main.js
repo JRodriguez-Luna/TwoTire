edcd384ba4d063a6dee70577360d74165ba897c5
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-09-30 22:19:24 +0000

    Add Modal and some design

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
+'use strict';
 const $openModal = document.querySelector('.openTodayModal');
 const $dismissModal = document.querySelector('.dismiss-modal');
 const $dialog = document.querySelector('dialog');
-
 if (!HTMLDialogElement.prototype.showModal) {
   console.warn('Browser does not support dialog.showModal');
 }
-
 $openModal?.addEventListener('click', () => {
   console.log('Button clicked!');
   $dialog?.showModal();
 });
-
 $dismissModal?.addEventListener('click', () => {
   console.log('Dialog dismissed!');
   $dialog?.close();
